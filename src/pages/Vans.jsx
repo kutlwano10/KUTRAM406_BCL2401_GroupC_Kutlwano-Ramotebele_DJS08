2e3756f5633f586da97cec3c2b3bc7fc77b7f7ab
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-06 11:58:09 +0200

    created a map() to get all the Vans List

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-export const Vans =()=> {
-    React.useEffect(()=> {
-        fetch("/api/vans")
-        .then(response=> response.json())
-        .then(data=>console.log(data))
-    }, [])
-    return(
-        <h1>Vans page goes here</h1>
-    )
-}
+export const Vans = () => {
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((response) => response.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  /**
+   * changed {} - () for the vans.map to not return anything
+   * when using {} it must return something
+   */
+  const vansElements = vans.map((van) => (
+    <div key={van.id} className="van-title">
+      <img src={van.imageUrl} alt={van.name}/>
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} Selected`}>{van.type}</i>
+    </div>
+  ));
+
+  return (
+    <div className="can-list-container">
+      <div className="van-list">{vansElements}</div>
+    </div>
+  );
+};
