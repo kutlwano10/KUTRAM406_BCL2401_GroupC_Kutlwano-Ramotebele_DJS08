ce709211276d056e896c6b3c3310283212c55c16
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-07 11:46:55 +0200

    Created Components{Header & Layout}
    -Nested all the child elements inside layout element \n -No path for Layout element ,to recognise the location of every page, used <Outlet/> for that

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,21 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Vans } from "./pages/Vans";
-import '/server'
+import "/server";
 import { VansDetails } from "./pages/VansDetails";
+import { Layout } from "../components/Layout";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VansDetails/>}/>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VansDetails />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
