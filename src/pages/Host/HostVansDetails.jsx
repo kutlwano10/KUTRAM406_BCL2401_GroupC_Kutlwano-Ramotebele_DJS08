a4b200160c9206cf20f5e80822de75b6c85d55a4
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-06-09 17:24:38 +0200

    Added HostVansDetails
    -Fetched api data using Id \n -Returns the data of the clicked Van from HostVans

--- src/pages/Host/HostVansDetails.jsx
+++ src/pages/Host/HostVansDetails.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useParams, Link, NavLink, Outlet } from "react-router-dom";
 
-export const HostVansDetails=()=>{
-    return (
-        <h1>Hello Kitty</h1>
-    )
-}
+export const HostVansDetails = () => {
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, []);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <section>
+      <Link to=".." relative="path" className="back-button">
+        &larr; <span>Back to all vans</span>
+      </Link>
+
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+        {/*Your changes will go here*/}
+      </div>
+    </section>
+  );
+};
